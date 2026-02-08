1e70b8d75bf305a032fc63ca9b79abe2e54e4590
AkshayP010 <akshaypoojari2000@gmail.com>
2026-02-08 18:11:51 +0530

    Fix image URLs to use public domain

--- backend-drf/products/serializers.py
+++ backend-drf/products/serializers.py
@@ -2,9 +2,11 @@ from rest_framework import serializers
 from .models import Product
 
 class ProductSerializers(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
+    
     class Meta:
         model=Product
-        fields="__all__"
+        fields = "__all__"
         
     def get_image(self, obj):
         if obj.image:
